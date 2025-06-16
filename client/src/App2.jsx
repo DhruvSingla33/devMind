32f22436976cbdbbf99fc69c56ea1da65432f2d1
Dhruv Singla <96688509+DhruvSingla33@users.noreply.github.com>
2025-06-16 23:12:16 +0530

    Update App2.jsx

--- client/src/App2.jsx
+++ client/src/App2.jsx
@@ -3,11 +3,13 @@ import DataProvider from './context/DataProvider';
 import Home1 from './components/Home';
 const App2 = () => {
   return (
+    <>
     <div  style={{ marginTop: '100px' }}>
     <DataProvider>    
     <Home1/>
   </DataProvider>
   </div>
+      </>
   )
 }
 
